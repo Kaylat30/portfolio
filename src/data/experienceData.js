00ab045d7eb26648813857b489e95fe00a1ec79b
Kaylat30 <kayondoabdulatif@gmail.com>
2024-07-17 20:56:44 +0300

    added GDSC

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -6,5 +6,19 @@ export const experienceData = [
         startYear: '2023',
         endYear: ''
     },
+    {
+        id: 2,
+        company: 'Mbarara University of Science and Technology',
+        jobtitle: 'Web Lead, Google Developer Student Club (GDSC)',
+        startYear: '2023',
+        endYear: '2024'
+    },
+    {
+        id: 3,
+        company: '',
+        jobtitle: 'Free Lancer',
+        startYear: '2022',
+        endYear: 'date'
+    },
     
 ]
