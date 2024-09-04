2af5bac3fe5d18f9d3b46aed2b31043c66e6ea9f
Kaylat30 <kayondoabdulatif@gmail.com>
2024-09-04 20:03:30 +0300

    updated experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,20 +1,27 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'Tech Things Ltd',
+        jobtitle: 'Systems Developer / Accounts Manager',
+        startYear: '2024',
+        endYear: 'date'
+    },
+    {
+        id: 2,
         company: 'Codinex Computers',
         jobtitle: 'Software Developer Intern',
         startYear: '2023',
         endYear: ''
     },
     {
-        id: 2,
+        id: 3,
         company: 'Mbarara University of Science and Technology',
         jobtitle: 'Web Lead, Google Developer Student Club (GDSC)',
         startYear: '2023',
         endYear: '2024'
     },
     {
-        id: 3,
+        id: 4,
         company: '',
         jobtitle: 'Free Lancer',
         startYear: '2022',
